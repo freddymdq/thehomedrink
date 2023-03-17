afb4caceda48407c1516f2ea988732b909f75450
Fede Mosquera <freddymdq@gmail.com>
2023-03-17 09:20:59 -0300

    Entrega TP 1 Mosquera Federico

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ class  ProductManager{
     existe (id) {
         return this.products.find((product) => product.id === id)
     }
-
+    
     // ternario
     getProductsById (id){
         !this.existe(id) ? console.log('Producto no encontrado') : console.log(this.existe(id));        
@@ -50,25 +50,25 @@ class  ProductManager{
 
 const productos = new ProductManager
 
-// Arreglo vacio llamada 1
+// Arreglo vacio 
 console.log(productos.getProducts())
 
-// Agregado Productos llamada 2
+// Agregado Productos 
 productos.addProduct('SWING', "Whisky deluxe", 18750, 'image', 'cod001', 21 )
-/* productos.addProduct('J.WALKER', "Whisky red label", 8750, 'image', 'cod002' ) */
+productos.addProduct('J.WALKER', "Whisky red label", 8750, 'image', 'cod002',33 )
+
+// No busca el producto (falta de datos.)
+productos.addProduct('ZACAPA', "Ron Destile deluxe reserve", 5750, 'image', 'cod004')
 
 // Arreglo con producto
 console.log(productos.getProducts())
 
-
 // Validacion de CODE duplicado.
  productos.addProduct('J.WALKER', "Whisky black label", 10750, 'image', 'cod002', 22 )
 
-
-
 // Buscamos por ID 
-/* productos.getProductsById(2) */
+productos.getProductsById(2)
 
 // Busqueda por ID no encontrado
-/* productos.getProductsById(3) */
+productos.getProductsById(4)
 
